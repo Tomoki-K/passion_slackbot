4b1f06298d4d3f50cb28127d0cfff0184e89c8de
Tomoki Kano <tomofuma@gmail.com>
2018-04-06 11:39:21 +0900

    initial commit

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strings"
+
+	"github.com/nlopes/slack"
+)
+
+func includes_passion(text string) bool {
+	keywords := [...]string{"パッション", "ぱっしょん", "passion"}
+	for _, e := range keywords {
+		if strings.Contains(text, e) {
+			return true
+		}
+	}
+	return false
+}
+
+func run(api *slack.Client) int {
+	rtm := api.NewRTM()
+	go rtm.ManageConnection()
+
+	for {
+		select {
+		case msg := <-rtm.IncomingEvents:
+			switch ev := msg.Data.(type) {
+			case *slack.HelloEvent:
+				log.Print("Hello Event")
+
+			case *slack.MessageEvent:
+				log.Printf("Message: %v\n", ev)
+				if includes_passion(ev.Text) {
+					rtm.SendMessage(rtm.NewOutgoingMessage("パッションが足りません。", ev.Channel))
+				}
+
+			case *slack.InvalidAuthEvent:
+				log.Print("Invalid credentials")
+				return 1
+
+			}
+		}
+	}
+}
+
+func main() {
+	api := slack.New(os.Getenv("SLACK_PASSION_KEY"))
+	os.Exit(run(api))
+}
